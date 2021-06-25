af8f6a3d33e5e9f8b96133f12709eb43e5f63941
nirali29 <nirali.bhardwaj29@gmail.com>
2021-06-25 21:53:18 +0530

    Auto-generated commit [ci skip]

---
+++ assets/stops.ts
@@ -0,0 +1,19 @@
+import { Stop } from 'src/app/interfaces/route.interface';
+
+export const STOPS: Stop[] = [
+  {
+    stopId: '101',
+    stopName: 'Jaipur',
+    latitude: 26.9124,
+    longitude: 75.7873
+},   {
+  stopId: '102',
+  stopName: 'Mumbai',
+  latitude: 19.0760,
+  longitude: 72.8777
+},
+    { stopId: '103', stopName: 'Sikkim', latitude: 27.5330, longitude: 88.5122 },
+    { stopId: '104', stopName: 'Udaipur', latitude: 24.5854, longitude: 73.7125 },
+    { stopId: '105', stopName: 'Chennai', latitude: 13.0827, longitude: 80.2707 },
+    { stopId: '106', stopName: 'Bangalore', latitude: 12.9716, longitude: 77.5946 }
+  ];
